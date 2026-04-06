681bf68a4f643242e73ebcd53b34f13d0bf692ec
GACH <germanach@educastur.org>
2026-04-06 12:50:44 +0200

    Refactor useReports hook to improve code formatting and maintainability

--- src/hooks/useReports.ts
+++ src/hooks/useReports.ts
@@ -1,9 +1,10 @@
-import {  useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { getReports } from '../api/jasper-report-api';
 
 export const useReports = () => {
 
   const [reports, setReports] = useState([]);
+  
   useEffect(() => {
     getReports()
       .then(data => { setReports(data as any);}).catch(e => console.log(e));
